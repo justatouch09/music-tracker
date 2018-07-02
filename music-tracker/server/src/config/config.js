dcceaf3b59f19462f01d049e201551035e798e21
Jarad Touchberry <Jaradtouch@gmail.com>
2018-07-02 17:15:36 -0400

    Added files from music tracker

---
+++ music-tracker/server/src/config/config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  port: process.env.PORT || 8081,
+  db: {
+    database: process.env.DB_NAME || 'musictracker',
+    user: process.env.DB_USER || 'musictracker',
+    password: process.env.DB_PASS || 'musictracker',
+    options: {
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: path.resolve(__dirname, '../../musictracker.sqlite')
+    }
+  },
+  authentication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
+  }
+}
